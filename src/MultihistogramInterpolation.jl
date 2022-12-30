d523d87363d4f8fa5cff992b83828a4725e88ea5
Cocoa <cocoathepenguin@protonmail.com>
2022-12-30 10:46:47 +0530

    Add function for absolute first and second moments

--- src/MultihistogramInterpolation.jl
+++ src/MultihistogramInterpolation.jl
@@ -49,7 +49,8 @@ function interpolate_energy(parameter_value, data::MultihistogramData; isbeta = 
     return exp(log(total) - F) + Omin
 end
 
-function interpolate_energy_second_moment(parameter_value, data::MultihistogramData; isbeta = false, returnlinear = false)
+function interpolate_energy_second_moment(parameter_value, data::MultihistogramData;
+        isbeta = false, returnlinear = false)
     total_linear = 0.
     total_square = 0.
     
@@ -120,6 +121,36 @@ function interpolate_observable(parameter_value, observable, data::Multihistogra
     return exp(log(total) - F) + Omin
 end
 
+function interpolate_observable_abs(parameter_value, observable, data::MultihistogramData; isbeta = false)
+    total = 0.
+    if !isbeta
+        β = 1/parameter_value
+    else
+        β = parameter_value
+    end
+    
+    Tvec = data.parameter_values
+    Evec = data.marginal_energy_histograms
+    F    = interpolate_free_energy(parameter_value, data)
+    u    = data.free_energies
+    J    = length(Tvec)
+    A    = -1/2 * (maximum(u) + minimum(u))
+
+    
+    for i in 1:J
+        for state in MHData.tuple_iterators[i]
+            if state.f == 0
+                continue
+            end
+            num = state.f * abs(getproperty(state, observable))
+            den = sum([ exp(-A-u[j]+(β - 1/Tvec[j]) * state.U) for j in 1:J])
+            total += num/den
+        end
+    end
+    
+    return exp(log(total) - F) + Omin
+end
+
 function interpolate_observable_second_moment(parameter_value, observable, data::MultihistogramData;
         isbeta = false, returnlinear = false)
     total_linear = 0.
@@ -163,4 +194,47 @@ function interpolate_observable_second_moment(parameter_value, observable, data:
         return Osq
     end
     
+end
+
+function interpolate_observable_second_moment_abs(parameter_value, observable, data::MultihistogramData;
+    isbeta = false, returnlinear = false)
+total_linear = 0.
+total_square = 0.
+
+if !isbeta
+    β = 1/parameter_value
+else
+    β = parameter_value
+end
+
+Tvec = data.parameter_values
+Evec = data.marginal_energy_histograms
+F    = interpolate_free_energy(parameter_value, data)
+u    = data.free_energies
+J    = length(Tvec)
+A    = -1/2 * (maximum(u) + minimum(u))
+
+for i in 1:J
+    for state in MHData.tuple_iterators[i]
+        if state.f == 0
+            continue
+        end
+        num_li = state.f * abs(getproperty(state, observable))
+        num_sq = num_li * abs(getproperty(state, observable))
+        den = sum([ exp(-A-u[j]+(β - 1/Tvec[j]) * state.U) for j in 1:J])
+        
+        total_linear += num_li/den
+        total_square += num_sq/den
+    end
+end
+
+Oli = exp(log(total_linear) - F)
+Osq = exp(log(total_square) - F)
+
+if returnlinear
+    return Oli, Osq
+else
+    return Osq
+end
+
 end
