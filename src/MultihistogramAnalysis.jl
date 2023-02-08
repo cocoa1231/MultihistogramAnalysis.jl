9ebb673b44b94cc5241178a0e226f09e2c6eff5c
Cocoa <cocoathepenguin@protonmail.com>
2023-02-09 02:06:13 +0530

    Add logsum functions

--- src/MultihistogramAnalysis.jl
+++ src/MultihistogramAnalysis.jl
@@ -10,16 +10,51 @@ export getrange,
     marginalvalue,
     marginalize,
     free_energy_iteration,
+    free_energy_iteration_logsum,
     calculate_free_energies!,
     interpolate_free_energy,
+    interpolate_energy_logsum,
     interpolate_energy,
+    interpolate_energy_logsum,
     interpolate_energy_second_moment,
+    interpolate_energy_second_moment_logsum,
     interpolate_observable,
+    interpolate_observable_logsum,
     interpolate_observable_abs,
+    interpolate_observable_abs_logsum,
     interpolate_observable_second_moment,
-    interpolate_observable_second_moment_abs
+    interpolate_observable_second_moment_logsum,
+    interpolate_observable_second_moment_abs,
+    interpolate_observable_second_moment_abs_logsum
 
 include("MultihistogramDataStructures.jl")
+include("MultihistogramInterpolation.jl")
+
+function free_energy_iteration_logsum(u, data::MultihistogramData)
+    J = length(data.parameter_values)
+    A = -1/2 * (maximum(u) + minimum(u))
+    Evec = data.marginal_energy_histograms
+    Tvec = data.parameter_values
+    
+    Fk = zeros(J)
+    for k in 1:J
+        total_k = 0.
+        terms = []
+        for i in 1:J
+            for (E, f) in Evec[i]
+                if f == 0
+                    continue
+                end
+                num = f
+                den = [-A-u[j]+(1/Tvec[k] - 1/Tvec[j])*E for j in 1:J]
+                push!(terms, log(num) - logsum(den))
+            end
+            total_k = logsum(terms)
+        end
+        Fk[k] += total_k
+    end
+    return Fk
+end
 
 function free_energy_iteration(u, data::MultihistogramData)
     J = length(data.parameter_values)
@@ -42,11 +77,17 @@ function free_energy_iteration(u, data::MultihistogramData)
     return Fk
 end
 
-function calculate_free_energies!(data::MultihistogramData; rtol = 1e-10)
+function calculate_free_energies!(data::MultihistogramData; rtol = 1e-10, logsum = false)
     P = ProgressThresh(rtol^2, "Minimizing: ")
     F = Fnew = zeros(length(data.parameter_values))
+
+    f = free_energy_iteration
+    if logsum
+        f = free_energy_iteration_logsum
+    end
+
     while true
-        Fnew = free_energy_iteration(F, data)
+        Fnew = f(F, data)
         err = sum( ((Fnew .- F) ./ F ) .^2 )
         ProgressMeter.update!(P, err)
         if err < rtol^2
@@ -58,6 +99,4 @@ function calculate_free_energies!(data::MultihistogramData; rtol = 1e-10)
     return Fnew
 end
 
-include("MultihistogramInterpolation.jl")
-
-end
+end # module MultihistogramAnalysis
--- src/MultihistogramInterpolation.jl
+++ src/MultihistogramInterpolation.jl
@@ -1,3 +1,11 @@
+function logsum(logterms)
+    lm = maximum(logterms)
+    logterms_removed = filter(!=(lm), logterms)
+    return lm + log1p(sum(exp.(logterms_removed .- lm)))
+end
+
+### Free Energy
+
 function interpolate_free_energy(parameter_value, data::MultihistogramData; isbeta = false)
     total = 0.
     if !isbeta
@@ -14,6 +22,9 @@ function interpolate_free_energy(parameter_value, data::MultihistogramData; isbe
     
     for i in 1:J
         for (E, f) in Evec[i]
+            if f == 0
+                continue
+            end
             num = f
             den = sum([exp(-A-u[j]+(β - 1/Tvec[j])*E) for j in 1:J])
             total += num/den
@@ -21,6 +32,37 @@ function interpolate_free_energy(parameter_value, data::MultihistogramData; isbe
     end
     return log(total)
 end
+
+function interpolate_free_energy_logsum(parameter_value, data::MultihistogramData; isbeta = false)
+    if !isbeta
+        β = 1/parameter_value
+    else
+        β = parameter_value
+    end
+    
+    Tvec = data.parameter_values
+    Evec = data.marginal_energy_histograms
+    u    = data.free_energies
+    J    = length(Tvec)
+    A    = -1/2 * (maximum(u) + minimum(u))
+    
+    logterms = Float64[]
+    for i in 1:J
+        for (E, f) in Evec[i]
+            if f == 0
+                continue
+            end
+            num = f
+            den = [-A-u[j]+(β - 1/Tvec[j])*E for j in 1:J]
+            push!(logterms, log(num) - logsum(den))
+        end
+    end
+
+    return logsum(logterms)
+end
+
+
+### Internal Energy
 
 function interpolate_energy(parameter_value, data::MultihistogramData; isbeta = false)
     total = 0.
@@ -41,12 +83,47 @@ function interpolate_energy(parameter_value, data::MultihistogramData; isbeta = 
     
     for i in 1:J
         for (E, f) in Evec[i]
+            if f == 0
+                continue
+            end
+
             num = f * (E - Omin)
             den = sum([exp(-A-u[j]+(β - 1/Tvec[j])*E) for j in 1:J])
             total += num/den
         end
     end
     return exp(log(total) - F) + Omin
+end
+
+function interpolate_energy_logsum(parameter_value, data::MultihistogramData; isbeta = false)
+    if !isbeta
+        β = 1/parameter_value
+    else
+        β = parameter_value
+    end
+    
+    Tvec = data.parameter_values
+    Evec = data.marginal_energy_histograms
+    F    = interpolate_free_energy(parameter_value, data)
+    u    = data.free_energies
+    J    = length(Tvec)
+    A    = -1/2 * (maximum(u) + minimum(u))
+    Oranges = (x -> x[:U]).(data.observable_ranges)
+    Omin = minimum(minimum.(Oranges))
+    
+    logterms = Float64[]
+    for i in 1:J
+        for (E, f) in Evec[i]
+            if f == 0
+                continue
+            end
+
+            num = f * (E - Omin)
+            den = [-A-u[j]+(β - 1/Tvec[j])*E for j in 1:J]
+            push!(logterms, log(num) - logsum(den))
+        end
+    end
+    return exp(logsum(logterms) - F) + Omin
 end
 
 function interpolate_energy_second_moment(parameter_value, data::MultihistogramData;
@@ -71,6 +148,10 @@ function interpolate_energy_second_moment(parameter_value, data::MultihistogramD
     
     for i in 1:J
         for (E, f) in Evec[i]
+            if f == 0
+                continue
+            end
+
             num_li = f * (E - Omin)
             num_sq = num_li * (E - Omin)
             
@@ -90,6 +171,51 @@ function interpolate_energy_second_moment(parameter_value, data::MultihistogramD
     end
 end
 
+function interpolate_energy_second_moment_logsum(parameter_value, data::MultihistogramData;
+        isbeta = false, returnlinear = false)    
+    if !isbeta
+        β = 1/parameter_value
+    else
+        β = parameter_value
+    end
+    
+    Tvec = data.parameter_values
+    Evec = data.marginal_energy_histograms
+    F    = interpolate_free_energy(parameter_value, data)
+    u    = data.free_energies
+    J    = length(Tvec)
+    A    = -1/2 * (maximum(u) + minimum(u))
+    Oranges = (x -> x[:U]).(data.observable_ranges)
+    Omin = minimum(minimum.(Oranges))
+    
+    logterms_linear = Float64[]
+    logterms_square = Float64[]
+    for i in 1:J
+        for (E, f) in Evec[i]
+            if f == 0
+                continue
+            end
+            num_li = f * (E - Omin)
+            num_sq = num_li * (E - Omin)
+            
+            den = logsum([-A-u[j]+(β - 1/Tvec[j])*E for j in 1:J])
+            push!(logterms_linear, log(num_li) - den)
+            push!(logterms_square, log(num_sq) - den)
+        end
+    end
+    
+    Oli = exp(logsum(logterms_linear) - F) + Omin
+    Osq = exp(logsum(logterms_square) - F) + 2 * Oli * Omin - Omin^2
+    
+    if returnlinear
+        return Oli, Osq
+    else
+        return Osq
+    end
+end
+
+### Arbitrary Observable ⟨M⟩
+
 function interpolate_observable(parameter_value, observable, data::MultihistogramData; isbeta = false)
     total = 0.
     if !isbeta
@@ -99,7 +225,6 @@ function interpolate_observable(parameter_value, observable, data::Multihistogra
     end
     
     Tvec = data.parameter_values
-    Evec = data.marginal_energy_histograms
     F    = interpolate_free_energy(parameter_value, data)
     u    = data.free_energies
     J    = length(Tvec)
@@ -119,6 +244,36 @@ function interpolate_observable(parameter_value, observable, data::Multihistogra
     end
     
     return exp(log(total) - F) + Omin
+end
+
+function interpolate_observable_logsum(parameter_value, observable, data::MultihistogramData; isbeta = false)
+    if !isbeta
+        β = 1/parameter_value
+    else
+        β = parameter_value
+    end
+    
+    Tvec = data.parameter_values
+    F    = interpolate_free_energy(parameter_value, data)
+    u    = data.free_energies
+    J    = length(Tvec)
+    A    = -1/2 * (maximum(u) + minimum(u))
+    Oranges = (x -> x[observable]).(data.observable_ranges)
+    Omin = minimum(minimum.(Oranges))
+    
+    logterms = Float64[]
+    for i in 1:J
+        for state in data.tuple_iterators[i]
+            if state.f == 0
+                continue
+            end
+            num = state.f * (getproperty(state, observable) - Omin)
+            den = [ -A-u[j]+(β - 1/Tvec[j]) * state.U for j in 1:J]
+            push!(logterms, log(num) - logsum(den))
+        end
+    end
+    
+    return exp(logsum(logterms) - F) + Omin
 end
 
 function interpolate_observable_abs(parameter_value, observable, data::MultihistogramData; isbeta = false)
@@ -150,6 +305,34 @@ function interpolate_observable_abs(parameter_value, observable, data::Multihist
     return exp(log(total) - F)
 end
 
+function interpolate_observable_abs_logsum(parameter_value, observable, data::MultihistogramData; isbeta = false)
+    if !isbeta
+        β = 1/parameter_value
+    else
+        β = parameter_value
+    end
+    
+    Tvec = data.parameter_values
+    F    = interpolate_free_energy(parameter_value, data)
+    u    = data.free_energies
+    J    = length(Tvec)
+    A    = -1/2 * (maximum(u) + minimum(u))
+
+    logterms = Float64[]
+    for i in 1:J
+        for state in data.tuple_iterators[i]
+            if state.f == 0
+                continue
+            end
+            num = state.f * abs(getproperty(state, observable))
+            den = [ -A-u[j]+(β - 1/Tvec[j]) * state.U for j in 1:J]
+            push!(logterms, log(num) - logterms(den))
+        end
+    end
+    
+    return exp(logsum(logterms) - F)
+end
+
 function interpolate_observable_second_moment(parameter_value, observable, data::MultihistogramData;
         isbeta = false, returnlinear = false)
     total_linear = 0.
@@ -162,7 +345,6 @@ function interpolate_observable_second_moment(parameter_value, observable, data:
     end
     
     Tvec = data.parameter_values
-    Evec = data.marginal_energy_histograms
     F    = interpolate_free_energy(parameter_value, data)
     u    = data.free_energies
     J    = length(Tvec)
@@ -195,6 +377,51 @@ function interpolate_observable_second_moment(parameter_value, observable, data:
     
 end
 
+function interpolate_observable_second_moment_logsum(parameter_value, observable, data::MultihistogramData;
+        isbeta = false, returnlinear = false)
+    
+    if !isbeta
+        β = 1/parameter_value
+    else
+        β = parameter_value
+    end
+    
+    Tvec = data.parameter_values
+    F    = interpolate_free_energy(parameter_value, data)
+    u    = data.free_energies
+    J    = length(Tvec)
+    A    = -1/2 * (maximum(u) + minimum(u))
+    Oranges = (x -> x[observable]).(data.observable_ranges)
+    Omin = minimum(minimum.(Oranges))
+    
+    logterms_linear = Float64[]
+    logterms_square = Float64[]
+    for i in 1:J
+        for state in data.tuple_iterators[i]
+            if state.f == 0
+                continue
+            end
+            num_li = state.f * (getproperty(state, observable) - Omin)
+            num_sq = num_li * (getproperty(state, observable) - Omin)
+            den = logsum([-A-u[j]+(β - 1/Tvec[j] * state.U) for j in 1:J])
+            
+            push!(logterms_linear, log(num_li) - den)
+            push!(logterms_square, log(num_sq) - den)    
+        end
+    end
+    
+    Oli = exp(logsum(logterms_linear) - F) + Omin
+    Osq = exp(logsum(logterms_square) - F) + 2 * Oli * Omin - Omin^2
+    
+    if returnlinear
+        return Oli, Osq
+    else
+        return Osq
+    end
+    
+end
+
+
 function interpolate_observable_second_moment_abs(parameter_value, observable, data::MultihistogramData;
         isbeta = false, returnlinear = false)
     total_linear = 0.
@@ -207,7 +434,6 @@ function interpolate_observable_second_moment_abs(parameter_value, observable, d
     end
 
     Tvec = data.parameter_values
-    Evec = data.marginal_energy_histograms
     F    = interpolate_free_energy(parameter_value, data)
     u    = data.free_energies
     J    = length(Tvec)
@@ -229,6 +455,49 @@ function interpolate_observable_second_moment_abs(parameter_value, observable, d
 
     Oli = exp(log(total_linear) - F)
     Osq = exp(log(total_square) - F)
+
+    if returnlinear
+        return Oli, Osq
+    else
+        return Osq
+    end
+end
+
+function interpolate_observable_second_moment_abs_logsum(parameter_value, observable, data::MultihistogramData;
+        isbeta = false, returnlinear = false)
+    total_linear = 0.
+    total_square = 0.
+
+    if !isbeta
+        β = 1/parameter_value
+    else
+        β = parameter_value
+    end
+
+    Tvec = data.parameter_values
+    F    = interpolate_free_energy(parameter_value, data)
+    u    = data.free_energies
+    J    = length(Tvec)
+    A    = -1/2 * (maximum(u) + minimum(u))
+
+    logterms_linear = Float64[]
+    logterms_square = Float64[]
+    for i in 1:J
+        for state in data.tuple_iterators[i]
+            if state.f == 0
+                continue
+            end
+            num_li = state.f * abs(getproperty(state, observable))
+            num_sq = num_li * abs(getproperty(state, observable))
+            den = logsum([ -A-u[j]+(β - 1/Tvec[j]) * state.U for j in 1:J])
+            
+            push!(logterms_linear, log(num_li) - den)
+            push!(logterms_square, log(num_sq) - den)
+        end
+    end
+
+    Oli = exp(logsum(logterms_linear) - F)
+    Osq = exp(logsum(logterms_square) - F)
 
     if returnlinear
         return Oli, Osq
