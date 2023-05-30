9bed1e5b621c9b63fc3444f12fe16dc0820c498e
Cocoa <cocoathepenguin@protonmail.com>
2023-05-30 18:58:42 +0530

    Change site name

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(;
     modules=[MultihistogramAnalysis],
     authors="Cocoa <cocoathepenguin@protonmail.com> and contributors",
     repo="https://github.com/cocoa1231/MultihistogramAnalysis.jl/blob/{commit}{path}#{line}",
-    sitename="MultihistogramAnalysis.jl",
+    sitename="Multihistogram Analysis",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://cocoa1231.github.io/MultihistogramAnalysis.jl",
